9fd14ae0845639ed1359973af121af7b3e01db36
Abdul Malik Ikhsan <samsonasik@gmail.com>
2017-01-18 00:07:19 +0700

    Module spec

--- spec/ModuleSpec.php
+++ spec/ModuleSpec.php
@@ -1,5 +1,24 @@
 <?php
 
+namespace ForceHttpsModuleSpec;
+
+use ForceHttpsModule\Module;
+
 describe('Module', function () {
-    
+
+    beforeAll(function () {
+        $this->module = new Module;
+    });
+
+    describe('->getConfig()', function () {
+
+        it('returns config', function () {
+
+            $expected = include 'config/module.config.php';
+            expect($this->module->getConfig())->toBe($expected);
+
+        });
+
+    });
+
 });
