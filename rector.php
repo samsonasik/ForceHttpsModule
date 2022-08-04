8cd9b7457ae86544b41cdb36ed17111e8fcbd281
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-08-04 21:25:00 +0700

    update rector

--- rector.php
+++ rector.php
@@ -6,7 +6,6 @@ use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
 use Rector\CodingStyle\Rector\ArrowFunction\StaticArrowFunctionRector;
 use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
 use Rector\Config\RectorConfig;
-use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
@@ -31,8 +30,5 @@ return static function (RectorConfig $rectorConfig): void {
         StaticClosureRector::class       => [
             __DIR__ . '/spec',
         ],
-        RemoveAlwaysTrueIfConditionRector::class => [
-            __DIR__ . '/src/HttpsTrait.php',
-        ]
     ]);
 };
