e44141fe9db1b294c0cad391050c842aa5b8689d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-01-11 08:07:35 +0700

    use strict for in_array() string route name

--- src/HttpsTrait.php
+++ src/HttpsTrait.php
@@ -35,10 +35,10 @@ trait HttpsTrait
 
         if ($this->config['force_all_routes']) {
             return ! (! empty($this->config['exclude_specific_routes'])
-            && in_array($matchedRouteName, $this->config['exclude_specific_routes']));
+            && in_array($matchedRouteName, $this->config['exclude_specific_routes'], true));
         }
 
-        return in_array($matchedRouteName, $this->config['force_specific_routes']);
+        return in_array($matchedRouteName, $this->config['force_specific_routes'], true);
     }
 
     /**
